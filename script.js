6e0dfcadcbea80fb91dbf37e7614d280acc5f670
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-04-21 11:32:21 +0530

    page 1 partial

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var timer=document.querySelector('#line-part1 h5');
+function loadingAnimation(){
+    var timer=document.querySelector('#line-part1 h5');
 var count=0;
 
 var tl=gsap.timeline();
@@ -12,7 +13,7 @@ tl.from('#loader h1',{
 tl.from('#line-part1 h5, #line-part1 h6,#now',{
     opacity:0,
     onStart:function(){        
-    var int=setInterval(() => {
+        var int=setInterval(() => {
     if(count<=100){
         count++;
         timer.innerHTML=count;
@@ -27,7 +28,7 @@ tl.from('#line-part1 h5, #line-part1 h6,#now',{
 tl.to('#loader',{
     opacity:0,
     duration:0.5,
-    delay:4,
+    delay:3,
 })
 tl.from('#page1',{
     y:1200,
@@ -39,3 +40,43 @@ tl.to('#loader',{
     duration:1,
     display:'none',
 })
+tl.from('#nav #nav-2',{
+    y:-150,
+    stagger:2,
+    duration:0.6,
+    delay:0,
+    opacity:0,
+})
+gsap.from('.hero h1',{
+    y:150,
+    stagger:0.1,
+    duration:0.6,
+    delay:6,
+})
+}
+loadingAnimation();
+
+
+// document.addEventListener('mousemove',function(pos){
+//     gsap.to('#cursor',{
+//         left:pos.x,
+//         top:pos.y,
+//         duration:0.1,
+//         delay:0.01,
+//     })
+// })
+
+function cursorAnimation(){
+    Shery.mouseFollower({
+        skew: true,
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.5,
+      });
+    
+      Shery.makeMagnet("#nav-2 h3" , {
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+      });
+}
+cursorAnimation();
+
